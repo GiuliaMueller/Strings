151def61cb077e23e4b81214d7cefec7cecb8770
Giulia Mueller <giulia_mueller@yahoo.com>
2018-05-15 13:05:39 -0400

    Wrapper Classes

--- Wrappers.java
+++ Wrappers.java
@@ -9,6 +9,9 @@ public class Wrappers {
 		byte b = 123;		short s = 123;			int i = 123;
 		long l = 123L;		float f = 123.450F;		double d = 123.456D;
 		
+		// Java provides Wrapper Classes for all primitive types
+		Character cw = new Character(c);
+		Boolean blw = new Boolean(b1);
 		
 		
 	}
