fb55801ac158d71ca464a6f42aef1124086e6875
Giulia Mueller <giulia_mueller@yahoo.com>
2018-05-15 13:14:39 -0400

    Max Values

--- Wrappers.java
+++ Wrappers.java
@@ -13,7 +13,22 @@ public class Wrappers {
 		Character cw = new Character(c);
 		Boolean blw = new Boolean(b1);
 		
-		
+		// Display Number Wrapper's max value
+		System.out.println("Byte range: from " + new Byte(b).MIN_VALUE +
+				" to " +new Byte(b).MAX_VALUE );
+		System.out.println("Short range: from " + new Short(s).MIN_VALUE +
+				" to " +new Short(s).MAX_VALUE );
+		System.out.println("Integer range: from " + new Integer(i).MIN_VALUE +
+				" to " +new Integer(i).MAX_VALUE );
+		System.out.println("Long range: from " + new Long(l).MIN_VALUE +
+				" to " +new Long(l).MAX_VALUE );
+		System.out.println("Float range: from " + new Float(f).MIN_VALUE +
+				" to " +new Float(f).MAX_VALUE );
+		System.out.println("Double range: from " + new Double(d).MIN_VALUE +
+				" to " +new Double(d).MAX_VALUE );
+		System.out.println();
+		System.out.println("Character max value: ");
+		System.out.println(new Character(c).MAX_VALUE + 0 );
 	}
 
 }
