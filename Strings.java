098d6ddc611ebde189992e8f4edb4b30db426256
Giulia Mueller <giulia_mueller@yahoo.com>
2018-05-14 23:09:24 -0400

    concatenating Strings with numbers and boolean

---
+++ Strings.java
@@ -0,0 +1,12 @@
+package chapter3;
+
+public class Strings {
+	public static void main(String[] args) {
+		// concatenating Strings with numbers and boolean
+		System.out.println(1 + "2" + 3);
+		System.out.println(1 + 2 + "3");
+		System.out.println("1" + 2 + 3);
+		System.out.println("1" + "2" +true+ 3);
+	}
+
+}
