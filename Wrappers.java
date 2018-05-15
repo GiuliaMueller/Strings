9f04e9e041948648f17b837670b2707e987d4657
Giulia Mueller <giulia_mueller@yahoo.com>
2018-05-15 13:02:30 -0400

    Numeric primitives

--- Wrappers.java
+++ Wrappers.java
@@ -4,6 +4,13 @@ public class Wrappers {
 	public static void main(String[] args) {
 		// Non-Number primitives
 		boolean b1 = true; 		char c = 'a';
+		
+		// Numeric primitives
+		byte b = 123;		short s = 123;			int i = 123;
+		long l = 123L;		float f = 123.450F;		double d = 123.456D;
+		
+		
+		
 	}
 
 }
