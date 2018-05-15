57f41d3c07a3d724487f95db00fb8fa0a2ea9506
Giulia Mueller <giulia_mueller@yahoo.com>
2018-05-15 12:54:18 -0400

    Non-Number primitives

---
+++ Wrappers.java
@@ -0,0 +1,9 @@
+package chapter3;
+import java.util.HashMap;
+public class Wrappers {
+	public static void main(String[] args) {
+		// Non-Number primitives
+		boolean b1 = true; 		char c = 'a';
+	}
+
+}
